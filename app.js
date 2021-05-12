2fde3a6f644f1475e5b9aa6cec37aa6011897be9
zorz0 <83619017+zorz0@users.noreply.github.com>
2021-05-12 09:11:20 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import { a, b } from './testing/assertion';
+
+if (a == b) {
+
+    console.log("a");
+
+
+    console.log("b");
+}
+for (let index = 0; index < array.ts(10); index++);
+const element = array[index];
